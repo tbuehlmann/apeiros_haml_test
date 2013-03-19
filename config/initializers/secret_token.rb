d8add5caa8b6d24c879ff9c01012f1bba844fa29
tbuehlmann <tobias.buehlmann@gmx.de>
2013-03-19 15:00:45 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApeirosHamlTest::Application.config.secret_token = 'f54f3c958c31dbcc3c46010f034ff35badee297f44e99d75fdb96c25a04a3ce0aac0b3f2ef89b1398022506baba511ba542ced17c2cb1fda36cd7d7f57960ab5'
